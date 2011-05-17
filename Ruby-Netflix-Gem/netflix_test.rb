2e996f06aba7d26c18da860ae99b7bd93fb53b04
Aditya Gavvala <agavvala@gmail.com>
2011-05-16 21:14:40 -0400

    Test case included

---
+++ Ruby-Netflix-Gem/netflix_test.rb
@@ -0,0 +1,26 @@
+require_relative "netflix"
+require "test/unit"
+
+class NetflixTest < MiniTest::Unit::TestCase
+
+  def setup
+    @netflix = Netflix::API.new("xyz", "abc")
+  end
+
+  def test_search_people
+    http_response = @netflix.search_people('joan')
+    puts "Response:"+http_response.code
+    puts http_response.body
+
+    assert !http_response.nil?
+  end
+
+  def test_get_person_by_id
+    http_response = @netflix.get_person_by_id(152672)
+    puts "Response:"+http_response.code
+    puts http_response.body
+
+    assert !http_response.nil?
+
+  end
+end
