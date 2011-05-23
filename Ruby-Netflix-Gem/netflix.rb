100ca96f4f648127874096ccc352110e93165474
Aditya Gavvala <agavvala@gmail.com>
2011-05-23 19:20:09 -0400

    Working netflix package

--- Ruby-Netflix-Gem/netflix.rb
+++ Ruby-Netflix-Gem/netflix.rb
@@ -1,10 +1,10 @@
 require "net/http"
 require "cgi"
 require "base64"
-require "hmac-sha1"               # make sure ruby-hmac gem is installed
+require "hmac-sha1" # make sure ruby-hmac gem is installed
 require 'rexml/document'
 require "xml/mapping"
-require_relative "catalog_titles.rb"
+require_relative "netflix-object-xml-mapping.rb"
 require_relative "extras"
 
 module Netflix
@@ -23,62 +23,71 @@ module Netflix
     end
 
     private
-      # generate a random number to be used as nonce
-      def nonce
-        rand(1_500_000_000)
-      end
+    # generate a random number to be used as nonce
+    def nonce
+      rand(1_500_000_000)
+    end
 
-      # url encode a string
-      def e(value)
-        CGI.escape(value)
-      end
+    # url encode a string
+    def e(value)
+      CGI.escape(value)
+    end
 
-      # url timestamp within 5 minutes +/-
-      def nonce_timestamp(tolerance = 5)
-        Time.now.to_i + tolerance
-      end
+    # url timestamp within 5 minutes +/-
+    def nonce_timestamp(tolerance = 5)
+      Time.now.to_i + tolerance
+    end
 
-      # this method adds the signature to the parameter map
-      def sign(method="GET", path="/", parameters={})
-        # add special oauth parameters to the incoming parameter hash map
-        parameters.merge!({
-                       oauth_consumer_key: service_consumer_key,
-                       oauth_version: OAUTH_VERSION,
-                       oauth_signature_method: SIGNATURE_ALGORITHM,
-                       oauth_timestamp: nonce_timestamp,
-                       oauth_nonce: nonce
-                     })
-        #puts parameters.parameter_string
-        to_sign_string = "#{method}&#{e NETFLIX_URL}#{e path}&#{e parameters.parameter_string}"
-        #puts ".OAuth Base String: "+to_sign_string
-        signature = Base64.encode64(HMAC::SHA1.digest("#{service_shared_secret}&", to_sign_string))
-        parameters[ :oauth_signature ] = e signature
-      end
+    # get document root
+    def doc_root(response)
+      body = response.body.gsub(/^<\?xml version\="1\.0" standalone\="yes"\?>/, "") # this line was giving us grief with REXML
+      REXML::Document.new(body).root
+    end
 
-      # HTTP methods
-      def get(path="/", parameters={})
-        sign("GET", path, parameters)
-        url_string = "#{NETFLIX_URL}#{path}?#{parameters.parameter_string}"
-        url = URI::parse(url_string)
-        http_response = Net::HTTP.start(url.host, url.port) { |http| http.get(url_string)}
-        http_response
-      end
+    # get object list
+    def get_object_list(class_name, response)
+       Netflix.const_get(class_name).send('load_from_xml', doc_root(response))
+    end
+
+    # this method adds the signature to the parameter map
+    def sign(method="GET", path="/", parameters={})
+      # add special oauth parameters to the incoming parameter hash map
+      parameters.merge!({
+                            oauth_consumer_key: service_consumer_key,
+                            oauth_version: OAUTH_VERSION,
+                            oauth_signature_method: SIGNATURE_ALGORITHM,
+                            oauth_timestamp: nonce_timestamp,
+                            oauth_nonce: nonce
+                        })
+      #puts parameters.parameter_string
+      to_sign_string = "#{method}&#{e NETFLIX_URL}#{e path}&#{e parameters.parameter_string}"
+      #puts ".OAuth Base String: "+to_sign_string
+      signature = Base64.encode64(HMAC::SHA1.digest("#{service_shared_secret}&", to_sign_string))
+      parameters[:oauth_signature] = e signature
+    end
+
+    # HTTP methods
+    def get(path="/", parameters={})
+      sign("GET", path, parameters)
+      url_string = "#{NETFLIX_URL}#{path}?#{parameters.parameter_string}"
+      url = URI::parse(url_string)
+      http_response = Net::HTTP.start(url.host, url.port) { |http| http.get(url_string) }
+      http_response
+    end
 
     public
-      # Search for people by some part of their name (term)
-      def search_people(term, start_index=0, max_results=25)
-        get("catalog/people", { term: term, start_index: start_index, max_results: max_results})
-      end
+    # Search for people by some part of their name (term)
+    def search_people(term, start_index=0, max_results=25)
+      get_object_list "CatalogTitles", get("catalog/people", {term: term, start_index: start_index, max_results: max_results})
+    end
 
-      # Get a person details by the person/person-id URL
-      def get_person_by_id(person_id)
-        get("catalog/people/#{person_id}")
-      end
+    # Get a person details by the person/person-id URL
+    def get_person_by_id(person_id)
+      get_object_list "Person", get("catalog/people/#{person_id}")
+    end
 
     def search_title(search_term, start_index=0, max_results=10)
-      response = get("catalog/titles", {term: search_term, start_index: start_index, max_results: max_results})
-      doc = REXML::Document.new(response.body)
-      CatalogTitles.load_from_xml(doc.root)
+      get_object_list "People", get("catalog/titles", {term: search_term, start_index: start_index, max_results: max_results})
     end
 
   end
--- Ruby-Netflix-Gem/netflix_test.rb
+++ Ruby-Netflix-Gem/netflix_test.rb
@@ -5,10 +5,10 @@ require "test/unit"
 class NetflixTest < MiniTest::Unit::TestCase
 
   def setup
-    @netflix = Netflix::API.new("abc", "xyz")
+    @netflix = Netflix::API.new("utqwghusfjzfqnybzrqyxq89", "yfVMDGQPjW")
   end
 
-  def test_search_people
+  def test1_search_people
     http_response = @netflix.search_people('joan')
     #puts "Response:"+http_response.code
     #puts http_response.body
@@ -16,7 +16,7 @@ class NetflixTest < MiniTest::Unit::TestCase
     assert !http_response.nil?
   end
 
-  def test_get_person_by_id
+  def test1_get_person_by_id
     http_response = @netflix.get_person_by_id(152672)
     #puts "Response:"+http_response.code
     #puts http_response.body
@@ -24,16 +24,25 @@ class NetflixTest < MiniTest::Unit::TestCase
     assert !http_response.nil?
 
   end
-
+          1
   def test_search_titles
-    response = @netflix.search_title("annamayya")
+#    response = @netflix.search_title("annamayya")
 
-    assert (response.results_per_page "10")
+#    assert (response.results_per_page "10")
   end
 
-  def test_marshall_titles
+  def test1_marshall_titles
     file = File.dirname($0) + '/title_sample.xml'
     titles = CatalogTitles.load_from_file(file)
     titles
   end
+
+  def test_search_people
+    #file = File.dirname($0) + '/people_sample.xml'
+    #people = People.load_from_file(file)
+    #people
+    people = @netflix.search_people("john")
+    puts people
+  end
+
 end
